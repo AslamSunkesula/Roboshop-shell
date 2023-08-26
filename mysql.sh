8cf06d8ed450e4dc47861fb2a1e3e957bdb127fd
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-26 21:55:39 +0530

    automating the mongod installation

--- mysql.sh
+++ mysql.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
+
 DATE=$(date +%F)
 LOGSDIR=/tmp
-script_name=$0
-LOGFILE=$LOGSDIR/$script_name-$DATE.log
-
-#LOGFILE=/tmp/$script_name-$DATE.log
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-
-if [ $USERID -ne 0 ]; then
-
-    echo -e
-    " $R Error : Please run the script with root access $N"
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
-
 fi
 
-VALIDATE() {
-
-    if [ $1 -ne 0 ]; then
-
-        echo -e " $2 is ...... $R Failure"
-
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
     else
-
-        echo -e " $2 is .......$G Success "
+        echo -e "$2 ... $G SUCCESS $N"
     fi
-
 }
-
 
 yum module disable mysql -y &>> $LOGFILE
 
