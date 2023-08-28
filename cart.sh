574b6fa71df4f2327022afea1c812a735018abfa
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-28 22:32:08 +0530

    automating web layer

--- cart.sh
+++ cart.sh
@@ -24,3 +24,114 @@ VALIDATE() {
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
+
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+ Add application User if not exist
+
+id roboshop &>> /dev/null
+if [[ $? -ne 0 ]]
+then
+    useradd roboshop
+    VALIDATE "User roboshop created"
+fi
+
+# This is a usual practice that runs in the organization. Lets setup an app directory if not exist
+
+DIR="/app"
+if [[ ! -d "$DIR" ]] 
+then
+    mkdir "$DIR"
+    VALIDATE "$DIR Creation"
+fi
+
+
+curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip
+
+VALIDATE $? "downloading cart artifact"
+
+cd /app 
+
+
+VALIDATE $? "Moving into app directory"
+
+unzip /tmp/cart.zip
+
+VALIDATE $? "unzipping cart"
+
+npm install 
+
+
+VALIDATE $? "Installing dependencies"
+
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
+useradd roboshop &>>$LOGFILE
+
+#write a condition to check directory already exist or not
+mkdir /app &>>$LOGFILE
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
+
+VALIDATE $? "downloading cart artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app directory"
+
+unzip /tmp/cart.zip &>>$LOGFILE
+
+VALIDATE $? "unzipping cart"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+# give full path of cart.service because we are inside /app
+cp /home/centos/Roboshop-shell/cart.service /etc/systemd/system/cart.service &>>$LOGFILE
