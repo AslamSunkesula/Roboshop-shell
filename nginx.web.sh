6219de3c8b1ce1163ea3048b8711ec2e2eccbda9
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-28 10:17:55 +0530

    modified the we service

--- nginx.web.sh
+++ nginx.web.sh
@@ -1,31 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 DATE=$(date +%F)
-SCRIPT_NAME="$0"
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-W="\033[0m"
+N="\e[0m"
+Y="\e[33m"
 
-if [[ $(id -u) -ne 0 ]]
+if [ $USERID -ne 0 ];
 then
-        echo -e "$R ERROR : Please run this sctipt with root user, swich to root and try $W"
-        exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
 fi
 
-VALIDATE()
-{
-    if [[ $? -ne 0 ]]
-        then
-                echo -e "$1 $R ..... Failure $W"
-                exit 2
-        else
-                echo -e "$1 $G ..... Success $W"
-        fi
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
-
-# Install Nginx
 
 yum install nginx -y &>>$LOGFILE
 
@@ -55,25 +55,10 @@ unzip /tmp/web.zip &>>$LOGFILE
 
 VALIDATE $? "unzipping web artifact"
 
-cp /home/centos/roboshop-shell/roboshop.con /etc/nginx/default.d/roboshop.conf  &>>$LOGFILE
+cp /home/centos/Roboshop-shell/roboshop.con /etc/nginx/default.d/roboshop.conf  &>>$LOGFILE
 
 VALIDATE $? "copying roboshop config"
 
 systemctl restart nginx  &>>$LOGFILE
 
 VALIDATE $? "Restarting Nginx"
-# Extract the frontend content
-
-cd /usr/share/nginx/html
-
-uunzip /tmp/web.zip >> $LOGFILE
-
-# Create Nginx Reverse Proxy Configuration
-
-cp  /home/centos/Roboshop-shell/roboshop.con /etc/nginx/default.d/roboshop.conf >> $LOGFILE
-
-# Restart Nginx Service to load the changes of the configuration
-
-systemctl restart nginx
-
-VALIDATE "Restarting nginx service"
