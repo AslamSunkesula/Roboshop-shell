94eb2b2f09ea208dda83dbda392889200b7a360f
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-29 18:46:00 +0530

    creating the all instances

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,6 +5,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq ""catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=" "
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0c1c6845264d0303b
+DOMAIN_NAME=aslamroboshop.online
 
 for i in ${NAMES[@]}; do
 
@@ -20,8 +21,21 @@ for i in ${NAMES[@]}; do
     echo "creating $i instance"
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    
+
 
     echo "created $i instance : $IP_ADDRESS"
+
+     aws route53 change-resource-record-sets --hosted-zone-id Z035342425855QKP40L24 --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 
 done
